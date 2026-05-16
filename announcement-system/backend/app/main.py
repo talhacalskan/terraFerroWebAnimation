996d67db92f2dfc04f4d28d2475176b1356a6516
talhacalskan <149869341+talhacalskan@users.noreply.github.com>
2026-05-16 10:41:38 +0300

    Cors ayarlari duzeltildi

--- announcement-system/backend/app/main.py
+++ announcement-system/backend/app/main.py
@@ -32,6 +32,8 @@ app = FastAPI(
 
 # 3. Güvenlik (CORS) Ayarları
 cors_origins = [
+    "https://terraferrotech.com",
+    "https://www.terraferrotech.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
